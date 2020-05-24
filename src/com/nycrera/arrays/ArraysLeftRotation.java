633e1c3e2dd65357bab52e2f47a2bc27bb2fb41e
nycrera91 <nycrera@gmail.com>
2020-05-24 22:13:33 +0300

    Arrays Left Rotation solution added.

--- src/com/nycrera/arrays/ArraysLeftRotation.java
+++ src/com/nycrera/arrays/ArraysLeftRotation.java
@@ -1,5 +1,7 @@
 package com.nycrera.arrays;
 
+import java.util.Arrays;
+
 /**
  * Created by Alperen Asa
  * Date: 24.05.2020
@@ -8,4 +10,27 @@ package com.nycrera.arrays;
  */
 
 public class ArraysLeftRotation {
+
+    public static void main(String[] args) {
+
+        int[] arr = new int[]{1,2,3,4,5};
+
+        System.out.println(Arrays.toString(rotLeft(arr, 4)));
+    }
+
+    public static int[] rotLeft(int[] a, int d) {
+
+        int[] rotLeftArray = new int[a.length];
+
+        for(int i = 0; i < a.length; i++) {
+            int newIndex = (i + (a.length - d)) % a.length;
+
+            rotLeftArray[newIndex] = a[i];
+
+        }
+
+        return rotLeftArray;
+
+    }
+
 }
