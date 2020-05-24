32fb9c7625f18eb1d7df778683d9e92b9fa8ba84
nycrera91 <nycrera@gmail.com>
2020-05-24 18:35:59 +0300

    Repeated String solution added.

--- src/com/nycrera/warmupChallanges/RepeatedString.java
+++ src/com/nycrera/warmupChallanges/RepeatedString.java
@@ -8,4 +8,50 @@ package com.nycrera.warmupChallanges;
  */
 
 public class RepeatedString {
+
+    public static void main(String[] args) {
+
+        System.out.println(repeatedString("a", 1000000000000L));
+
+    }
+
+    public static long repeatedString(String s, Long n) {
+
+        long numberOfA = 0L;
+
+        if(n < s.length()) {
+            for(int i = 0; i < n; i++) {
+                if(s.toCharArray()[i] == 'a') {
+                    numberOfA++;
+                }
+            }
+
+            return numberOfA;
+        }
+
+        for (char letter : s.toCharArray()) {
+            if(letter == 'a') {
+                numberOfA++;
+            }
+        }
+
+        long divided =  (n / s.length());
+        long remain =  (n % s.length());
+
+        if(divided != 0) {
+            numberOfA *= divided;
+        }
+
+        char[] temp = s.toCharArray();
+
+        for(int i = 0; i < remain; i++) {
+            if(temp[i] == 'a') {
+                numberOfA++;
+            }
+        }
+
+        return numberOfA;
+
+    }
+
 }
